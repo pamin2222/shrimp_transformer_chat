3c8e6abe90010ede203b1e500b5dd525d9d76275
Pamin Rangsikunpum <>
2024-05-19 16:59:09 +0800

    Check key from st

--- key_helper.py
+++ key_helper.py
@@ -4,6 +4,8 @@ import os
 def check_openai_key(openai_api_key):
     if 'OPENAI_API_KEY' in os.environ:
         return
+    elif st.secrets["OPENAI_API_KEY"]:
+        os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
     elif not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
