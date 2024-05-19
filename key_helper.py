0ad26c025d2de125eb28bbeacc9c81bd84d52426
Pamin Rangsikunpum <>
2024-05-19 16:57:11 +0800

    revert

--- key_helper.py
+++ key_helper.py
@@ -5,8 +5,7 @@ def check_openai_key(openai_api_key):
     if 'OPENAI_API_KEY' in os.environ:
         return
     elif not openai_api_key:
-        #st.info("Please add your OpenAI API key to continue.")
-        #st.stop()
-        pass
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
     else:
         os.environ['OPENAI_API_KEY'] = openai_api_key
